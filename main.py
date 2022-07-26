c049e5106abd031c7d55906f773c7b249ec80f16
Craig Gidney <craiggidney@google.com>
2022-07-25 23:14:20 -0700

    Grab existing tools

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import stim
+from _builder import Builder
+
+
 def main():
-    print("hello world")
+    builder = Builder.for_qubits([1, 1j, 2])
+    builder.gate("H", [1, 1j, 2])
+    print(builder.circuit)
 
 
 if __name__ == '__main__':
